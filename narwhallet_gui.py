2cf8e2e52602fdc91e12b87d00aa3cbe2d0ff90d
canrnd <canrnd@outlook.com>
2021-11-27 07:08:01 -0700

    wip adding style sheet for qt visuals

--- narwhallet_gui.py
+++ narwhallet_gui.py
@@ -2,6 +2,7 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication
 from narwhallet.control import NarwhalletController
+from narwhallet.control.shared import MShared
 from narwhallet.core.kui.main import NarwhalletUI
 
 
@@ -11,6 +12,13 @@ def main():
     # TODO: pass override args to controller such as settings file path
     _program_path = os.path.dirname(__file__)
     ctr = NarwhalletController(view, _program_path, NarwhalletMain.clipboard())
+
+    _qss = MShared.get_resource_path('style.css')
+    _style = ''
+    with open(_qss, 'r', encoding='utf-8') as style_file:
+        _style = style_file.read()
+
+    NarwhalletMain.setStyleSheet(_style)
 
     view.show()
     xit = NarwhalletMain.exec_()
