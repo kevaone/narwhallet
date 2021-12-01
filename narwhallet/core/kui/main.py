3acb4d9fa2b98deb5b3d1c9444b46995fd71d266
canrnd <canrnd@outlook.com>
2021-11-30 17:22:37 -0700

    added version file

--- narwhallet/core/kui/main.py
+++ narwhallet/core/kui/main.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication as QApp
 from PyQt5.QtWidgets import QMainWindow
 
+from narwhallet._version import __version__
 from narwhallet.control.shared import MShared
 from narwhallet.core.kui.ux.main_window import Ui_MainWindow
 
@@ -19,7 +20,7 @@ class NarwhalletUI(QMainWindow):
 
         self.setObjectName('MainWindow')
         self.resize(858, 806)
-        self.setWindowTitle('Narwhallet')
+        self.setWindowTitle('Narwhallet ' + __version__)
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
