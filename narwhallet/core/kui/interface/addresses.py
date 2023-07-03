db32af5969d41f0709dec9fe9fccd2ca682e8533
canrnd <canrnd@outlook.com>
2023-07-03 12:51:14 -0600

    wire in address increase button

--- narwhallet/core/kui/interface/addresses.py
+++ narwhallet/core/kui/interface/addresses.py
@@ -1,16 +1,18 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty, ColorProperty)
+from kivy.uix.gridlayout import GridLayout
 from narwhallet.core.kui.widgets.addresslistinfo import AddressListInfo
-from kivy.graphics import Color, Rectangle
+from narwhallet.core.kui.widgets.nwlabel import Nwlabel
 
 
 class AddressesScreen(Screen):
-    address_list = ObjectProperty(None)
+    address_list = GridLayout()
+    wallet_name = Nwlabel()
 
     def __init__(self, **kwargs):
         super(AddressesScreen, self).__init__(**kwargs)
 
     def populate(self, wallet_name):
+        self.wallet_name.text = wallet_name.text
         self.address_list.clear_widgets()
         
         _w = self.manager.wallets.get_wallet_by_name(wallet_name.text)
@@ -21,33 +23,37 @@ class AddressesScreen(Screen):
                     _a = AddressListInfo()
                     _a.address.text = address.address
                     _a.address_label.text = address.label
-                    _a.balance.text = str(round(address.balance, 8)) #str(address.balance)
+                    _a.balance.text = str(round(address.balance, 8))
                     _a.transactions.text = str(len(address.history))
                     _a.wallet_name = wallet_name
                     _a.sm = self.manager
 
-                    with _a.canvas:
-                        Color(rgba=(125/255, 127/255, 127/255, 1))
-                        # Color(1, 0, 0, 1)  # set the colour to red
-                        # Rectangle(pos=self.pos,
-                        #           size=(self.size))
-                    # _a.canvas.ask_update()
                     self.address_list.add_widget(_a)
             for address in _w.change_addresses.addresses:
                 if address is not None:
                     _a = AddressListInfo()
                     _a.address.text = address.address
                     _a.address_label.text = address.label
-                    _a.balance.text = str(round(address.balance, 8)) #str(address.balance)
+                    _a.balance.text = str(round(address.balance, 8))
                     _a.transactions.text = str(len(address.history))
                     _a.wallet_name = wallet_name
                     _a.sm = self.manager
 
-                    with _a.canvas:
-                        Color(rgba=(0, 1, 0, 1))
-                        self.rect = Rectangle(pos=self.center,
-                                  size=(self.width/2.,
-                                        self.height/2.))
                     self.address_list.add_widget(_a)
         self.address_list.parent.scroll_y = 1
         self.manager.current = 'addresses_screen'
+
+    def increase_address_pool(self):
+        _w = self.manager.wallets.get_wallet_by_name(self.wallet_name.text)
+        _addr = _w.get_unused_address()
+        self.manager.wallets.save_wallet(_w.name)
+        _a = AddressListInfo()
+        _a.address.text = _addr
+        _a.address_label.text = ''
+        _a.balance.text = '0.0'
+        _a.transactions.text = '0'
+        _a.wallet_name = _w.name
+        _a.sm = self.manager
+
+        self.address_list.add_widget(_a)
+        self.manager.wallet_screen.btn_addresses.text = 'Addresses (' + str(_w.account_index + _w.change_index) + ')'
