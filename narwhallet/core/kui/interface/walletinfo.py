5bfbb52bb7f04903faa5ea7cf2795b40add74441
canrnd <canrnd@outlook.com>
2023-07-03 07:36:05 -0600

    quick walletinfo refinements

--- narwhallet/core/kui/interface/walletinfo.py
+++ narwhallet/core/kui/interface/walletinfo.py
@@ -1,22 +1,25 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
+from narwhallet.core.kcl.wallet import MWallet
 from narwhallet.control.shared import MShared
+from narwhallet.core.kui.widgets.nwlabel import Nwlabel
 
 class WalletInfoScreen(Screen):
-    wallet_name = ObjectProperty(None)
-    mnemonic_phrase = ObjectProperty(None)
-    seed = ObjectProperty(None)
-    ypub = ObjectProperty(None)
-    coin = ObjectProperty(None)
-    kind = ObjectProperty(None)
-    account_index = ObjectProperty(None)
-    change_index = ObjectProperty(None)
-    balance = ObjectProperty(None)
-    locked_balance = ObjectProperty(None)
-    last_updated = ObjectProperty(None)
+    wallet_name = Nwlabel()
+    mnemonic_phrase = Nwlabel()
+    seed = Nwlabel()
+    ypub = Nwlabel()
+    xpriv = Nwlabel()
+    coin = Nwlabel()
+    bip = Nwlabel()
+    kind = Nwlabel()
+    account_index = Nwlabel()
+    change_index = Nwlabel()
+    balance = Nwlabel()
+    locked_balance = Nwlabel()
+    last_updated = Nwlabel()
 
     def populate(self, name):
-        _w = self.manager.wallets.get_wallet_by_name(name.text)
+        _w: MWallet = self.manager.wallets.get_wallet_by_name(name.text)
 
         if _w is not None:
             # _w.bip
@@ -24,8 +27,10 @@ class WalletInfoScreen(Screen):
             self.wallet_name.text = _w.name
             self.mnemonic_phrase.text = _w.mnemonic
             self.seed.text = _w.seed
-            self.ypub.text = '_w.extended_pub'
+            self.ypub.text = _w.extended_pub
+            self.xpriv.text = _w.extended_prv
             self.coin.text = _w.coin
+            self.bip.text = _w.bip
             self.kind.text = _w.kind.name
             self.account_index.text = str(_w.account_index)
             self.change_index.text = str(_w.change_index)
@@ -33,3 +38,19 @@ class WalletInfoScreen(Screen):
             self.locked_balance.text = str(round(_w.locked, 8)) #str(_w.locked)
             self.last_updated.text = MShared.get_timestamp(_w.last_updated)[1]
         self.manager.current = 'walletinfo_screen'
+
+    def clear_screen(self):
+        self.wallet_name.text = ''
+        self.mnemonic_phrase.text = ''
+        self.seed.text = ''
+        self.ypub.text = ''
+        self.xpriv.text = ''
+        self.coin.text = ''
+        self.bip.text = ''
+        self.kind.text = ''
+        self.account_index.text = ''
+        self.change_index.text = ''
+        self.balance.text = ''
+        self.locked_balance.text = ''
+        self.last_updated.text = ''
+        self.manager.current = 'wallet_screen'
