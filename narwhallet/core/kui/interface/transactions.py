6f8436bd83fcc141507fcafffd022ad266e1a50e
canrnd <canrnd@outlook.com>
2023-07-13 00:00:32 -0600

    improving transactions screen load time; ux wip

--- narwhallet/core/kui/interface/transactions.py
+++ narwhallet/core/kui/interface/transactions.py
@@ -1,12 +1,13 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.recycleview import RecycleView
 from narwhallet.core.kui.widgets.transactionlistinfo import TransactionListInfo
 from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
 from narwhallet.core.kui.widgets.header import Header
 
 
 class TransactionsScreen(Screen):
-    transaction_list = GridLayout()
+    transaction_list = RecycleView()
     header = Header()
 
     def tx_value(self, txid):
@@ -15,46 +16,42 @@ class TransactionsScreen(Screen):
         if _asa is not None:
             for o in _asa.vout:
                 _value += o.value
-                # _o.value.text = str(round(o.value, 8)) #str(o.value)
         return _value
 
     def populate(self, wallet_name):
-        self.transaction_list.clear_widgets()
+        self.transaction_list.children[0].rows = 0
+        self.transaction_list.data = []
         self.transaction_list.parent.scroll_y = 1
         _w = self.manager.wallets.get_wallet_by_name(wallet_name)
         self.header.value = wallet_name
         _transactions = 0
         _ustx = 0
-        self.transaction_list.rows = 0
 
         _tx = {}
         if _w is not None:
             for address in _w.addresses.addresses:
                 if address is not None:
                     for _h in address.history:
-                        # TODO Check if tx_hash already present then fix the items below
-                        _t = TransactionListInfo()
-                        _t.transaction.text = _h['tx_hash']
-                        _t.block.text = str(_h['height'])
-                        # TODO Fix
-                        _t.txvalue.text = str(round(self.tx_value(_h['tx_hash']), 8))
-                        _t.sm = self.manager
-                        _t.status.text = 'Spent'
-                        _tx[_h['tx_hash']] = _t
+                        _d = {
+                                'transaction': _h['tx_hash'],
+                                'block': str(_h['height']),
+                                'txvalue': str(round(self.tx_value(_h['tx_hash']), 8)),
+                                'sm': self.manager,
+                                'status': 'Spent'}
+                        _tx[_h['tx_hash']] = _d
                     for _u in address.unspent_tx:
                         if _u['tx_hash'] in list(_tx.keys()):
-                            _tx[_u['tx_hash']].status.text = 'Partial Spend'
+                            _tx[_u['tx_hash']]['status'] = 'Partial Spend'
                             # TODO Include in above correction
-                            _tx[_u['tx_hash']].txvalue.text = str(round(_u['value']/10000000, 8))
+                            _tx[_u['tx_hash']]['txvalue'] = str(round(_u['value']/10000000, 8))
                         else:
-                            _t = TransactionListInfo()
-                            _t.transaction.text = _u['tx_hash']
-                            _t.block.text = str(_u['height'])
-                            # TODO Include in above correction
-                            _t.txvalue.text = str(round(_u['value']/10000000, 8))
-                            _t.sm = self.manager
-                            _t.status.text = 'Unspent'
-                            _tx[_u['tx_hash']] = _t
+                            _d = {
+                                'transaction': _h['tx_hash'],
+                                'block': str(_h['height']),
+                                'txvalue': str(round(self.tx_value(_h['tx_hash']), 8)),
+                                'sm': self.manager,
+                                'status': 'Unspent'}
+                            _tx[_u['tx_hash']] = _d
                         _ustx += 1
 
             # Change
@@ -62,35 +59,35 @@ class TransactionsScreen(Screen):
                 for address in _w.change_addresses.addresses:
                     if address is not None:
                         for _h in address.history:
-                            # TODO Check if tx_hash already present then fix the items below
-                            _t = TransactionListInfo()
-                            _t.transaction.text = _h['tx_hash']
-                            _t.block.text = str(_h['height'])
-                            # TODO Fix
-                            _t.txvalue.text = str(round(self.tx_value(_h['tx_hash']), 8))
-                            _t.sm = self.manager
-                            _t.status.text = 'Spent'
-                            _tx[_h['tx_hash']] = _t
+                            _d = {
+                                'transaction': _h['tx_hash'],
+                                'block': str(_h['height']),
+                                'txvalue': str(round(self.tx_value(_h['tx_hash']), 8)),
+                                'sm': self.manager,
+                                'status': 'Spent'}
+                            _tx[_h['tx_hash']] = _d
                         for _u in address.unspent_tx:
                             if _u['tx_hash'] in list(_tx.keys()):
-                                _tx[_u['tx_hash']].status.text = 'Partial Spend'
+                                _tx[_u['tx_hash']]['status'] = 'Partial Spend'
                                 # TODO Include in above correction
-                                _tx[_u['tx_hash']].txvalue.text = str(round(_u['value']/10000000, 8))
+                                _tx[_u['tx_hash']]['txvalue'] = str(round(_u['value']/10000000, 8))
                             else:
-                                _t = TransactionListInfo()
-                                _t.transaction.text = _u['tx_hash']
-                                _t.block.text = str(_u['height'])
-                                # TODO Include in above correction
-                                _t.txvalue.text = str(round(_u['value']/10000000, 8))
-                                _t.sm = self.manager
-                                _t.status.text = 'Unspent'
-                                _tx[_u['tx_hash']] = _t
+                                _d = {
+                                'transaction': _h['tx_hash'],
+                                'block': str(_h['height']),
+                                'txvalue': str(round(self.tx_value(_h['tx_hash']), 8)),
+                                'sm': self.manager,
+                                'status': 'Unspent'}
+                                _tx[_u['tx_hash']] = _d
                             _ustx += 1
 
+            _txd = []
+
             for _k, _v in _tx.items():
-                self.transaction_list.rows += 1
-                # self.transaction_list.rows_minimum[self.transaction_list.rows] = 65
-                self.transaction_list.add_widget(_v)
+                self.transaction_list.children[0].rows += 1
+                _txd.append(_v)
                 _transactions += 1
+
+            self.transaction_list.data = _txd
 
         self.manager.current = 'transactions_screen'
--- narwhallet/core/kui/widgets/transactionlistinfo.py
+++ narwhallet/core/kui/widgets/transactionlistinfo.py
@@ -1,17 +1,18 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager
+from kivy.properties import StringProperty
 from narwhallet.core.kui.widgets.nwlabel import Nwlabel
 
 
 class TransactionListInfo(BoxLayout):
-    transaction = Nwlabel()
-    block = Nwlabel()
+    transaction = StringProperty()
+    block = StringProperty()
     sm = ScreenManager()
-    status = Nwlabel()
-    txvalue = Nwlabel()
+    status = StringProperty()
+    txvalue = StringProperty()
 
     def on_touch_down(self, touch):
         if self.collide_point(touch.x, touch.y):
-            self.sm.transaction_screen.populate(self.transaction.text)
+            self.sm.transaction_screen.populate(self.transaction)
             return
         return super(TransactionListInfo, self).on_touch_down(touch)
