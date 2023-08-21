42ce6588d1a61305469d677bfb5790972039fb1e
canrnd <canrnd@outlook.com>
2023-08-20 18:20:40 -0600

    favorite and favorites screen updates

--- narwhallet/core/kui/interface/favorite.py
+++ narwhallet/core/kui/interface/favorite.py
@@ -1,42 +1,56 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from narwhallet.control.shared import MShared
 from narwhallet.core.kui.widgets.header import Header
+from narwhallet.core.kui.widgets.namespaceinfo import NamespaceInfo
+from narwhallet.core.kui.widgets.nwnsimage import Nwnsimage
+from narwhallet.core.kui.widgets.nwlabel import Nwlabel
 
 
 class FavoriteScreen(Screen):
-    namespaceid = ObjectProperty(None)
-    shortcode = ObjectProperty(None)
-    namespace_key_list = ObjectProperty(None)
-    creator = ObjectProperty(None)
-    namespace_name = ObjectProperty(None)
+    namespaceid = Nwlabel()
+    shortcode = Nwlabel()
+    namespace_key_list = BoxLayout()
+    creator = Nwlabel()
+    namespace_name = Nwlabel()
+    owner = Nwlabel()
     header = Header()
 
     def populate(self, namespaceid):
         self.header.value = 'Favorite'
-        self.namespace_key_list.scroll_y = 1
-        # self.header.value = self.manager.wallet_screen.header.value
-        self.namespace_key_list.data = []
-        _ns = self.manager.cache.ns.get_namespace_by_id(namespaceid)
+        self.namespace_key_list.parent.scroll_y = 1
+        self.namespace_key_list.clear_widgets()
         self.namespaceid.text = namespaceid
-        self.namespace_name.text = ''
+        _provider = self.manager.settings_screen.settings.content_providers[0]
+        _ns = MShared.get_namespace(namespaceid, _provider)
+        _ns = _ns['result']
+        self.namespace_name.text = str(_ns['name'])
+        self.shortcode.text = str(_ns['root_shortcode'])
+        self.owner.text = ''
+        self.creator.text = ''
+        _dat = _ns['data']
+        _dat.reverse()
 
-        for ns in _ns:
-            _dns = {} #NamespaceInfo()
-            _dns['key'] = str(ns[5])
-            _dns['data'] = str(ns[6])
-            _dns['sm'] = self.manager
+        for _kv in _dat:
+            _dns = NamespaceInfo()
+            _dns.key = str(_kv['dkey'])
+            _dns.data = str(_kv['dvalue'])
+            _dns.sm = self.manager
 
-            if ns[4] == 'OP_KEVA_NAMESPACE':
-                self.creator.text = ns[8]
-                self.shortcode.text = str(len(str(ns[0]))) + str(ns[0]) + str(ns[1])
-                if self.namespace_name.text == '':
-                    self.namespace_name.text = ns[6]
-            # elif ns[4] == 'OP_KEVA_PUT':
-            if ns[5] == '\x01_KEVA_NS_':
-                if self.namespace_name.text == '':
-                    self.namespace_name.text = ns[6]
+            if self.owner.text == '':
+                self.owner.text = _kv['addr']
+
+            if _kv['op'] == 'KEVA_NAMESPACE':
+                self.creator.text = _kv['addr']
+
+            _ipfs_images = self.manager.cache_IPFS(_dns.data)
             
-            self.owner.text = ns[8]
+            self.namespace_key_list.add_widget(_dns)
+            for _i in _ipfs_images:
+                _im = Nwnsimage()
+                _im.image_path = _i
+                self.namespace_key_list.add_widget(_im)
+
+            _ipfs_images = self.manager.cache_IPFS(_dns.data)
             
-            self.namespace_key_list.data.append(_dns)
         self.manager.current = 'favorite_screen'
--- narwhallet/core/kui/interface/favorites.py
+++ narwhallet/core/kui/interface/favorites.py
@@ -1,10 +1,11 @@
+import json
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.uix.recycleview import RecycleView
 from narwhallet.core.kui.widgets.header import Header
 
 
 class FavoritesScreen(Screen):
-    favorites_list = ObjectProperty(None)
+    favorites_list = RecycleView()
     header = Header()
 
     def populate(self):
@@ -17,11 +18,17 @@ class FavoritesScreen(Screen):
                 if p[0] == favorite:
                     _block = self.manager.cache.ns.ns_block(p[0])[0]
                     _ns_name = self.manager.cache.ns.ns_root_value(p[0])[0][0]
+
+                    try:
+                        _name = json.loads(_ns_name)['displayName']
+                    except:
+                        _name = _ns_name
+
                     _fav = 'narwhallet/core/kui/assets/star.png'
                     _ns = {
                         'address': p[0],
                         'shortcode': str(len(str(_block[0])))+str(_block[0])+str(_block[1]),
-                        'ns_name': str(_ns_name),
+                        'ns_name': str(_name),
                         'keys': str(self.manager.cache.ns.key_count(p[0])[0][0]),
                         'sm': self.manager,
                         'favorite_source': _fav}
