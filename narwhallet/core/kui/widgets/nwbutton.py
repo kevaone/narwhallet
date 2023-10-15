29ee0348a0649347f3cb08d9880532ee54a85cdf
canrnd <canrnd@outlook.com>
2023-10-14 21:48:30 -0600

    refine button hover

--- narwhallet/core/kui/widgets/nwbutton.py
+++ narwhallet/core/kui/widgets/nwbutton.py
@@ -22,13 +22,19 @@ class Nwbutton(Button):
         if self.collide_point(pos[0], pos[1]):
             if self.hover is False and self.disabled is False:
                 self.background_color = [146/255, 149/255, 149/255, 1]
-                Window.set_system_cursor('hand')
+                # Window.set_system_cursor('hand')
                 self.hover = True
         else:
-            if self.hover is True:
+            if self.hover is True and self.disabled is False:
                 self.background_color = [54/255, 58/255, 59/255, 1]
-                Window.set_system_cursor('arrow')
+                # Window.set_system_cursor('arrow')
                 self.hover = False
+
+    def on_hover(self, *args):
+        if self.hover is True:
+            Window.set_system_cursor('hand')
+        else:
+            Window.set_system_cursor('arrow')
 
     def _bind(self, dt):
         app = App.get_running_app()
