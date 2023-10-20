e4f3aa3cb0ccc45e6356cb2a6d28fe7c8f6721cc
canrnd <canrnd@outlook.com>
2023-10-19 21:15:57 -0600

    create popup for send confirmations

---
+++ narwhallet/core/kui/widgets/nwsendpopup.py
@@ -0,0 +1,40 @@
+import json
+from kivy.uix.modalview import ModalView
+from narwhallet.core.kex.KEXc import KEXclient
+from narwhallet.control.shared import MShared
+from narwhallet.core.kui.widgets.nwpopup import Nwpopup
+from kivy.properties import StringProperty
+
+
+class Nwsendpopup(ModalView):
+    provider = KEXclient()
+    in_value = StringProperty()
+    out_value = StringProperty()
+    change_value = StringProperty()
+    fee = StringProperty()
+    fee_rate = StringProperty()
+    txsize = StringProperty()
+    txhex = StringProperty()
+    return_screen = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(Nwsendpopup, self).__init__(**kwargs)
+
+    def process_send(self):
+        _bc_result = MShared.broadcast(self.txhex, self.provider)
+        if isinstance(_bc_result[1], dict):
+            _result = json.dumps(_bc_result[1])
+        else:
+            _result = _bc_result[1]
+
+        msgType = int(_bc_result[0])
+
+        result_popup = Nwpopup()
+
+        if msgType == 1:
+            result_popup.status._text = 'Error' + ':\n' + _result
+        elif msgType == 2:
+            result_popup.status._text = 'Ok!'
+
+        result_popup.open()
+        self.dismiss()
